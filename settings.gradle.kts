5d9e2a80d37fcdb393ff694f1b02e31f044f17e9
Thomas Broyer <t.broyer@ltgt.net>
2024-02-04 17:43:25 +0100

    Migrate to Develocity injection on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,5 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
-    id("com.gradle.enterprise") version "3.11.4"
-}
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-    }
-}
 
 rootProject.name = "gradle-errorprone-plugin"
