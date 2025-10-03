238bbc5e427d9bd9c666bf832d1ca96fe37f9293
Thomas Broyer <t.broyer@ltgt.net>
2025-10-03 11:18:16 +0200

    Limit JVM target to the published variant (main source set)
    This will compile tests to use the same target as the current
    JVM, and more importantly will change test dependencies'
    resolution to a variant compatible with the current JVM rather
    than with the plugin's target. This will allow updating JUnit
    Jupiter to version 6 which requires Java 17, and would fail to
    resolve when targetting Java 8.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `java-gradle-plugin`
@@ -15,10 +14,10 @@ plugins {
 group = "net.ltgt.gradle"
 
 // Make sure Gradle Module Metadata targets the appropriate JVM version
-tasks.withType<JavaCompile>().configureEach {
+tasks.compileJava {
     options.release = 8
 }
-tasks.withType<KotlinCompile>().configureEach {
+tasks.compileKotlin {
     // See https://jakewharton.com/kotlins-jdk-release-compatibility-flag/
     compilerOptions.freeCompilerArgs.add("-Xjdk-release=1.8")
     compilerOptions.jvmTarget = JvmTarget.JVM_1_8
